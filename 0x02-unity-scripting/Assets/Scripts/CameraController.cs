1bfcf94acee0e1227064287a9e63a272d393ab7b
lemejiamo <77980644+lemejiamo@users.noreply.github.com>
2021-12-22 19:24:45 -0500

    camera controller script

---
+++ 0x02-unity-scripting/Assets/Scripts/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+    public Transform camera;
+    public Vector3 ofset = new Vector3(1f, 40f, -20f);
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = camera.position + ofset;
+    }
+}
