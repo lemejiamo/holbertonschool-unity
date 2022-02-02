0540ae9f64e43edb9b335f759aaee33f78ac603e
lemejiamo <77980644+lemejiamo@users.noreply.github.com>
2022-02-02 17:27:58 -0500

    update player controller
    update mechanism system for CharacterController.

--- 0x05-unity-assets_models_textures/Assets/Scripts/PlayerController.cs
+++ 0x05-unity-assets_models_textures/Assets/Scripts/PlayerController.cs
@@ -4,51 +4,59 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // objects to controll movement
+    // public objs 
+    // for debuging purposes
     public float horizontalMove;
     public float verticalMove;
     public float speed;
+    public float jumpForce;
+    public float gravityValue;
 
-    public Vector3 playerDirection;
+    // private objs
+    private float fallVelocity;
 
-    // objects to control the camera move
+    // objects to control the camera
     private Camera mainCamera;
     private Vector3 camForward;
     private Vector3 camRigth;
 
     // stores the vector transfrom to applies the movement
     public Vector3 playerInput;
+    public Vector3 playerDirection;
 
     // instance from required classes
     public CharacterController player;
 
-    
     // Start is called before the first frame update
     void Start()
     {
+        // instance required objects
         player = GetComponent<CharacterController>();
-        mainCamera = FindObjectOfType<Camera>(MainCamera);
+        // init set
+        mainCamera = FindObjectOfType<Camera>();
+        gravityValue = 9.81f;
+        jumpForce = 10f;
+        speed = 30f;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Gets the input 
-        horizontalMove = Input.GetAxis("Horizontal");
-        verticalMove = Input.GetAxis("Vertical");
-        // Create the movement vector
-        playerInput = new Vector3(horizontalMove, 0, verticalMove);
-        playerInput = Vector3.ClampMagnitude(playerInput, 1);
-
+        // DO THE MATH FIRST
+        // get's camera possition
         CameraDirection();
+        // gets the final move vector
+        Move();
+        // applies gravity force to the object 
+        SetGravity();
+        // checks if the player wanna jump
+        Jump();
 
-        playerDirection = playerInput.x * camRigth + playerInput.z * camForward;
-
-        // applies the movement
-        player.Move(playerDirection * speed * Time.deltaTime);
-        print(mainCamera);
+        // execute the move
+        MovePlayer();
     }
-
+    // gets camera position
     void CameraDirection()
     {
         camForward = mainCamera.transform.forward;
@@ -60,10 +68,52 @@ public class PlayerController : MonoBehaviour
         camForward = camForward.normalized;
         camRigth = camRigth.normalized;
     }
+    
+    // Function to set the vector to applies move
+    void Move()
+    {
+        // Gets the input 
+        horizontalMove = Input.GetAxis("Horizontal");
+        verticalMove = Input.GetAxis("Vertical");
+        // Create the movement vector
+        playerInput = new Vector3(horizontalMove, 0, verticalMove);
+        playerInput = Vector3.ClampMagnitude(playerInput, 1);
+        // applies cam direction correction to player 
+        playerDirection = playerInput.x * camRigth + playerInput.z * camForward;
+        // aplies the speed to direction vector
+        playerDirection = playerDirection * speed;
+    }
 
-
-    private void FixedUpdate()
+    //function to set the gravity
+    void SetGravity()
     {
 
+        if (player.isGrounded)
+        {
+            fallVelocity = -gravityValue * Time.deltaTime;
+            playerDirection.y = fallVelocity;
+        }
+        else
+        {
+            fallVelocity -= gravityValue * Time.deltaTime;
+            playerDirection.y = fallVelocity;
+        }
+
+    }
+    
+    // Jump Skill
+    private void Jump()
+    {
+        if (Input.GetKey("space") && player.isGrounded)
+        {
+            fallVelocity = jumpForce;
+            playerDirection.y = fallVelocity;
+        }
+    }
+
+    private void MovePlayer()
+    {
+        // make the  move
+        player.Move(playerDirection * Time.deltaTime);
     }
 }
